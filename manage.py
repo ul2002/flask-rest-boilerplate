6b262a8c7ea8ec8d8ab1bfd749f49a1c97c440be
Ntella <ulrichsoft2002@gmail.com>
2019-02-27 11:56:20 +0100

    updates'

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ from app.main.service.logger_service import logger
 app = create_app('dev')
 
 log = logger()
-log.error('Error in /app/main/service/user_service.py : function save_new_user does not exist ');
+log.info('Server started ');
 
 app.register_blueprint(blueprint)
 
